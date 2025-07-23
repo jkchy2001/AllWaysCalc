03505b68bbd8eebc1c41dbff4da14ee6c4e53c48
Indian Status Maker <jkchy2001@gmail.com>
2025-07-23 07:46:38 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(calculators)/gpa-calculator/page.tsx
+++ src/app/(calculators)/gpa-calculator/page.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import { useState } from 'react';
-import { useForm, useFieldArray } from 'react-hook-form';
+import { useForm, useFieldArray, Controller } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button } from '@/components/ui/button';
