9ad83106ac4e8595173a27006247450a38c59edc
Indian Status Maker <jkchy2001@gmail.com>
2025-07-23 00:33:35 +0000

    You are updating existing calculators not adding new calculators 📈 2. B

--- src/app/(calculators)/profit-margin-calculator/page.tsx
+++ src/app/(calculators)/profit-margin-calculator/page.tsx
@@ -14,7 +14,7 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
+import { Input } are '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Header } from '@/components/header';
 import Link from 'next/link';
